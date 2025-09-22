92e31648d3c38214b0cde9dd1a25c17b625d388c
Sarthak Dhembare <165639215+SarthakTD@users.noreply.github.com>
2025-09-22 17:43:46 +0530

    adming mapping

--- jewelry-shop-frontend/src/api/http.js
+++ jewelry-shop-frontend/src/api/http.js
@@ -1,7 +1,7 @@
 // src/api/http.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE || 'http://localhost:8080';
+const baseURL = process.env.REACT_APP_API_BASE || 'https://jewelry-admin-1.onrender.com';
 
 const http = axios.create({
   baseURL
